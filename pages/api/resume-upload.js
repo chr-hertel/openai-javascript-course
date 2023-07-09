e2544a931e8ba4f356e3f99a415bd12ee0d6202a
Christopher Hertel <mail@christopher-hertel.de>
2023-07-09 20:30:41 +0200

    RoboHR challenge

--- pages/api/resume-upload.js
+++ pages/api/resume-upload.js
@@ -16,6 +16,7 @@ import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { PineconeClient } from "@pinecone-database/pinecone";
 import { loadSummarizationChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
+import { load } from "cheerio";
 
 export default async function handler(req, res) {
   // Grab the prompt from the url (?prompt=[value])
@@ -25,6 +26,75 @@ export default async function handler(req, res) {
   // Always use a try catch block to do asynchronous requests and catch any errors
   try {
     // do stuff
+    const directory =
+      "/home/christopher/Playground/LangChain/openai-javascript-course/data/resumes";
+    const loader = new DirectoryLoader(directory, {
+      ".pdf": (path) => new PDFLoader(path),
+    });
+    const docs = await loader.load();
+    // Splitt the documents with their metadata
+    const splitter = new CharacterTextSplitter({
+      separator: " ",
+      chunkSize: 200,
+      chunkOverlap: 20,
+    });
+
+    const splitDocs = await splitter.splitDocuments(docs);
+
+    // reduce the metadata
+    const reducedDocs = splitDocs.map((doc) => {
+      const fileName = doc.metadata.source.split("/").pop();
+      const [_, firstName, lastName] = fileName.split("_");
+
+      return {
+        ...doc,
+        metadata: {
+          first_name: firstName,
+          last_name: lastName.slice(0, -4),
+          docType: "resume",
+        },
+      };
+    });
+
+    let summaries = [];
+    const model = new OpenAI({ temperature: 0 });
+    const summarizeAllChain = loadSummarizationChain(model, {
+      type: "map_reduce",
+    });
+
+    const summarizeRes = await summarizeAllChain.call({
+      input_documents: docs,
+    });
+    summaries.push({ summary: summarizeRes.text });
+
+    // Summarize each candidate
+    for (let doc of docs) {
+      const summarizeOneChain = loadSummarizationChain(model, {
+        type: "map_reduce",
+      });
+
+      const summarizeOneRes = await summarizeOneChain.call({
+        input_documents: [doc],
+      });
+
+      summaries.push({ summary: summarizeOneRes.text });
+    }
+
+    // Upload the reducedDocs
+    const client = new PineconeClient();
+    await client.init({
+      apiKey: process.env.PINECONE_API_KEY,
+      environment: process.env.PINECONE_ENVIRONMENT,
+    });
+    const index = client.Index(process.env.PINECONE_INDEX);
+    await PineconeStore.fromDocuments(reducedDocs, new OpenAIEmbeddings(), {
+      pineconeIndex: index,
+    });
+
+    console.log("Uploaded to pinecone");
+
+    const summaryStr = JSON.stringify(summaries, null, 2);
+    return res.status(200).json({ output: summaryStr });
   } catch (err) {
     // If we have an error
 
